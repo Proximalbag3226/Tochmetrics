981aa3c64e1862e12d064be6c52f8ed9a12ebf1f
Luis Enrique <perezcoronaluisenrique4@gmail.com>
2024-05-28 02:17:19 -0600

    Idea de reportes

--- src/pages/estadisticas.jsx
+++ src/pages/estadisticas.jsx
@@ -1,8 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Button } from 'reactstrap';
-import DataTable from '../components/date_table';
-import ModalForm from '../components/modal_form';
 import Cards from '../components/cards';
 import '../components/cards.css'
 
--- src/pages/reportes.jsx
+++ src/pages/reportes.jsx
@@ -1,12 +1,59 @@
-import * as BsIcons from "react-icons/bs";
+import React from "react";
+import { Container, UncontrolledAccordion, AccordionItem, AccordionHeader, AccordionBody} from "reactstrap";
 
 function Reportes(){
     return(
-        <>
-            <h1>Aqui se mostraran los reportes</h1>
-            <br />
-            <BsIcons.BsPlusCircleFill/>
-        </>
+        <div className="noc">
+        <Container>
+        <UncontrolledAccordion defaultOpen="1">
+  <AccordionItem>
+    <AccordionHeader targetId="1">
+      Accordion Item 1
+    </AccordionHeader>
+    <AccordionBody accordionId="1">
+      <strong>
+        This is the first item's accordion body.
+      </strong>
+      You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the{' '}
+      <code>
+        .accordion-body
+      </code>
+      , though the transition does limit overflow.
+    </AccordionBody>
+  </AccordionItem>
+  <AccordionItem>
+    <AccordionHeader targetId="2">
+      Accordion Item 2
+    </AccordionHeader>
+    <AccordionBody accordionId="2">
+      <strong>
+        This is the second item's accordion body.
+      </strong>
+      You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the{' '}
+      <code>
+        .accordion-body
+      </code>
+      , though the transition does limit overflow.
+    </AccordionBody>
+  </AccordionItem>
+  <AccordionItem>
+    <AccordionHeader targetId="3">
+      Accordion Item 3
+    </AccordionHeader>
+    <AccordionBody accordionId="3">
+      <strong>
+        This is the third item's accordion body.
+      </strong>
+      You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the{' '}
+      <code>
+        .accordion-body
+      </code>
+      , though the transition does limit overflow.
+    </AccordionBody>
+  </AccordionItem>
+</UncontrolledAccordion>
+        </Container>
+        </div>
     );
 };
 
