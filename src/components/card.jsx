08083582930e60843f3d40a364ee575ff350ecf7
Luis Enrique <perezcoronaluisenrique4@gmail.com>
2024-05-29 01:22:51 -0600

    Formulario_reportes

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Card, CardBody, CardTitle, CardText, Button } from 'reactstrap';
-import img1 from'../Img/american_futbol.jpg'
-
+import ola from '../Img/campo_americano5.jpg'
 function Card1({ data, click }) {
+  var min = 1;
+  var max = 4;
+
+  var x = Math.floor(Math.random()*(max-min+1)+min);
+  const imageSrc = `../Img/campo_americano${x}.jpg`
+
+console.log(x);
   return (
     <Card style={{ width: '18rem' }}>
-      <img alt="Sample" src={img1} />
+      <img alt="Sample" src={ola} />
       <CardBody>
         <CardTitle tag="h5">{`Partido ${data.partido}`}</CardTitle>
         <CardText>{`Lugar: ${data.lugar}`}</CardText>
--- src/pages/reportes.jsx
+++ src/pages/reportes.jsx
@@ -1,11 +1,56 @@
-import React from "react";
-import { Container, UncontrolledAccordion, AccordionItem, AccordionHeader, AccordionBody} from "reactstrap";
+import React, { useState } from "react";
+import { Container, UncontrolledAccordion, AccordionItem, AccordionHeader, AccordionBody, Button} from "reactstrap";
+import * as BsIcons from "react-icons/bs";
+import ModalForm from "../components/modal_form";
+
+const initialData = [
+  { partido: 1, lugar: 'CDMX', fecha: 'Hoy', equipos: "Ny vs Ravens", description: "Hola este es un reporte"}
+];
+
+const fields = [
+  { name: "partido", type: "text" },
+  { name: "lugar", type: "text" },
+  { name: "fecha", type: "date" },
+  {name: "equipos", type: "text" },
+  {name: "description", type: "text"}
+];
 
 function Reportes(){
+  const [data, setData] = useState(initialData);
+  const [modalInsertar, setModalInsertar] = useState(false);
+  const [modalVerMas, setModalVerMas] = useState(false);
+  const [form, setForm] = useState({ partido: "", lugar: "", fecha: "", hora: "", anot: "", ob: "", sack: "", inter: "", pex: "", panot: "" });
+  const [currentData, setCurrentData] = useState({});
+
+  const mostrarModalInsertar = () => setModalInsertar(true);
+
+  const cerrarModalInsertar = () => setModalInsertar(false);
+
+  const mostrarModalVerMas = (item) => {
+    setCurrentData(item);
+    setModalVerMas(true);
+  };
+
+  const cerrarModalVerMas = () => setModalVerMas(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const insertar = () => {
+    const nuevoPartido = { ...form, partido: data.length + 1 };
+    setData([...data, nuevoPartido]);
+    setModalInsertar(false);
+    setForm({ partido: "", lugar: "", fecha: "", hora: "", anot: "", ob: "", sack: "", inter: "", pex: "", panot: "" }); // Reset form after insertion
+  };
+
     return(
         <div className="noc">
         <Container>
-            <h1>Reportes</h1>
+            <h1>Reportes <Button color="success" onClick={mostrarModalInsertar}>
+            <BsIcons.BsPlusCircleFill/>
+          </Button></h1>
             <br/>
             <br/>
         <UncontrolledAccordion defaultOpen="1">
@@ -55,6 +100,14 @@ function Reportes(){
     </AccordionBody>
   </AccordionItem>
 </UncontrolledAccordion>
+<ModalForm
+isOpen={modalInsertar}
+title="Nuevo reporte"
+fields={fields}
+formData={{...form, partido: data.length + 1}}
+onChange={handleChange}
+onSave={insertar}
+onClose={cerrarModalInsertar}/>
         </Container>
         </div>
     );
